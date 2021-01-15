5eb193ad6fd2fffcbc1661fcb0ff38e4c2844ba3
manuegp <manuel.mgp.2001@gmail.com>
2021-01-15 09:49:04 +0100

    Añadiendo la libreria php-crud-api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Psr\Http\Message\ServerRequestInterface;
+use Tqdev\PhpCrudApi\Api;
+use Tqdev\PhpCrudApi\Config;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::any('/{any}', function (ServerRequestInterface $request) {
+    $config = new Config([
+    	'address' => env('DB_HOST'),
+        'username' => env('DB_USERNAME'),
+        'password' => env('DB_PASSWORD'),
+        'database' => env('DB_DATABASE'),
+        'basePath' => '/api',
+    ]);
+    $api = new Api($config);
+    $response = $api->handle($request);
+    return $response;
+})->where('any', '.*');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
